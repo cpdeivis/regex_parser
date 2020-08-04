f0a90ea4fc595927a58839630a9b4c1b9ea530a7
oscaruno <dcpereira@inf.ufsm.br>
2020-08-03 22:54:20 -0300

    Remove Tokens Tokens sem implementação na gramática

--- RElex.py
+++ RElex.py
@@ -4,28 +4,26 @@ from sly import Lexer
 class RegexLexer(Lexer):
     tokens = {
         ESCAPE,
-        LBRACE,
-        RBRACE,
         LBRACK,
         RBRACK,
         LPAREN,
         RPAREN,
         F_STAR,
         F_PLUS,
-        QMARK,
         DOT,
+        ALT,
         CHAR,
     }
     ESCAPE = r"\\."
-    LBRACE = r"\{"
-    RBRACE = r"\}"
+    # LBRACE = r"\{"
+    # RBRACE = r"\}"
     LBRACK = r"\["
     RBRACK = r"\]"
     LPAREN = r"\("
     RPAREN = r"\)"
     F_STAR = r"\*"
     F_PLUS = r"\+"
-    QMARK = r"\?"
+    # QMARK = r"\?"
     DOT = r"\."
     ALT = r"\|"
     CHAR = r"."
