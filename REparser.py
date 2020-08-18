5c070dc3f2136219a06209730232a151b467c4e4
oscaruno <dcpereira@inf.ufsm.br>
2020-08-18 02:09:16 -0300

    Refatoração: - Ajusta o código para usar a classe eNFA; - Adiciona a flag "Verbose".

--- REparser.py
+++ REparser.py
@@ -1,46 +1,49 @@
 from sly import Parser
 from RElex import RegexLexer
-from Automata import State
+from Automata import eNFA
+
 
 class RegexParser(Parser):
     tokens = RegexLexer.tokens
 
-    def __init__(self):
-        self.eAlphabet = set([])
-        self.eStates = {}
-        State._counter = 0
+    def __init__(self, isVerbose=False):
+        self.Automaton = eNFA()
+        self.Verbose = isVerbose
 
     @_("union", "simple_re")
     def re(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         if "union" in p._namemap:
+            self.Automaton.Start = p.union[0]
             return p.union
         else:
+            self.Automaton.Start = p.simple_re[0]
             return p.simple_re
 
     @_("re ALT simple_re")
     def union(self, p):
-        print(p._slice)
-        start = State(False)
-        end = State(True)
+        if self.Verbose:
+            print(p._slice)
+        start = self.Automaton.createState(False)
+        end = self.Automaton.createState(True)
         f_start, f_end = p.re
         s_start, s_end = p.simple_re
 
-        start.addEpsilonTransition(f_start)
-        start.addEpsilonTransition(s_start)
+        self.Automaton.addEpsilonTransition(start, f_start)
+        self.Automaton.addEpsilonTransition(start, s_start)
 
-        f_end.isEnd = False
-        s_end.isEnd = False
-        f_end.addEpsilonTransition(end)
-        s_end.addEpsilonTransition(end)
+        self.Automaton.End.remove(f_end)
+        self.Automaton.End.remove(s_end)
+        self.Automaton.addEpsilonTransition(f_end, end)
+        self.Automaton.addEpsilonTransition(s_end, end)
 
-        self.eStates[start.name] = start
-        self.eStates[end.name] = end
         return (start, end)
 
     @_("concatenation", "basic_re")
     def simple_re(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         if "concatenation" in p._namemap:
             return p.concatenation
         else:
@@ -48,18 +51,20 @@ class RegexParser(Parser):
 
     @_("simple_re basic_re")
     def concatenation(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         f_start, f_end = p.simple_re
         s_start, s_end = p.basic_re
 
-        f_end.isEnd = False
-        f_end.addEpsilonTransition(s_start)
+        self.Automaton.End.remove(f_end)
+        self.Automaton.addEpsilonTransition(f_end, s_start)
 
         return (f_start, s_end)
-    
+
     @_("star", "plus", "question", "elementary_re")
     def basic_re(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         if "star" in p._namemap:
             return p.star
         elif "plus" in p._namemap:
@@ -71,99 +76,100 @@ class RegexParser(Parser):
 
     @_("elementary_re F_STAR")
     def star(self, p):
-        print(p._slice)
-        start = State(False)
-        end = State(True)
+        if self.Verbose:
+            print(p._slice)
+        start = self.Automaton.createState(False)
+        end = self.Automaton.createState(True)
         p_start, p_end = p.elementary_re
 
-        p_end.isEnd = False
-        start.addEpsilonTransition(end)
-        start.addEpsilonTransition(p_start)
-        p_end.addEpsilonTransition(end)
-        p_end.addEpsilonTransition(p_start)
+        self.Automaton.End.remove(p_end)
+        self.Automaton.addEpsilonTransition(start, end)
+        self.Automaton.addEpsilonTransition(start, p_start)
+        self.Automaton.addEpsilonTransition(p_end, end)
+        self.Automaton.addEpsilonTransition(p_end, p_start)
 
-        self.eStates[start.name] = start
-        self.eStates[end.name] = end
         return (start, end)
 
     @_("elementary_re F_PLUS")
     def plus(self, p):
-        print(p._slice)
-        end = State(True)
+        if self.Verbose:
+            print(p._slice)
+        end = self.Automaton.createState(True)
         start, p_end = p.elementary_re
 
-        p_end.isEnd = False
-        p_end.addEpsilonTransition(end)
-        end.addEpsilonTransition(start)
+        self.Automaton.End.remove(p_end)
+        self.Automaton.addEpsilonTransition(p_end, end)
+        self.Automaton.addEpsilonTransition(end, start)
 
-        self.eStates[end.name] = end
         return (start, end)
 
     @_("elementary_re QMARK")
     def question(self, p):
-        print(p._slice)
-        start = State(False)
-        end = State(True)
+        if self.Verbose:
+            print(p._slice)
+        start = self.Automaton.createState(False)
+        end = self.Automaton.createState(True)
         p_start, p_end = p.elementary_re
 
-        p_end.isEnd = False
-        start.addEpsilonTransition(end)
-        start.addEpsilonTransition(p_start)
-        p_end.addEpsilonTransition(end)
+        self.Automaton.End.remove(p_end)
+        self.Automaton.addEpsilonTransition(start, end)
+        self.Automaton.addEpsilonTransition(start, p_start)
+        self.Automaton.addEpsilonTransition(p_end, end)
 
-        self.eStates[start.name] = start
-        self.eStates[end.name] = end
         return (start, end)
 
     # @_("group", "ESCAPE", "DOT", "CHAR", "set")
     @_("group", "ESCAPE", "CHAR", "set")
     def elementary_re(self, p):
-        print(p._slice)
-        symbol =  p.ESCAPE if "ESCAPE" in p._namemap else (p.CHAR if "CHAR" in p._namemap else None)
+        if self.Verbose:
+            print(p._slice)
+        symbol = (
+            p.ESCAPE
+            if "ESCAPE" in p._namemap
+            else (p.CHAR if "CHAR" in p._namemap else None)
+        )
         if symbol:
-            start = State(False)
-            end = State(True)
-            start.addSymbolTransition(symbol, end)
+            start = self.Automaton.createState(False)
+            end = self.Automaton.createState(True)
+            self.Automaton.addSymbolTransition(symbol, start, end)
 
-            self.eAlphabet.add(symbol)
-            self.eStates[start.name] = start
-            self.eStates[end.name] = end
             return (start, end)
 
         return p.group if "group" in p._namemap else p.set
 
     @_("LPAREN re RPAREN")
     def group(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         return p.re
 
     @_("LBRACK set_items RBRACK")
     def set(self, p):
-        print(p.set_items)
-        start = State(False)
-        end = State(True)
+        if self.Verbose:
+            print(p.set_items)
+        start = self.Automaton.createState(False)
+        end = self.Automaton.createState(True)
         for item in p.set_items:
-            start.addSymbolTransition(item, end)
+            self.Automaton.addSymbolTransition(item, start, end)
 
-        self.eStates[start.name] = start
-        self.eStates[end.name] = end
         return (start, end)
 
     @_("set_items set_item")
     def set_items(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         p.set_items.append(p.set_item)
         return p.set_items
-            
+
     @_("set_item")
     def set_items(self, p):
-        return [ p.set_item ]
+        return [p.set_item]
 
     # @_("ESCAPE", "DOT", "CHAR")
     @_("ESCAPE", "CHAR")
     def set_item(self, p):
-        print(p._slice)
+        if self.Verbose:
+            print(p._slice)
         symbol = p.CHAR if "CHAR" in p._namemap else p.ESCAPE
 
-        self.eAlphabet.add(symbol)
         return symbol
